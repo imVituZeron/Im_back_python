b9b6aa1cf9b4a9e030573ff563f2a7c8dbaa5728
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-07 11:18:38 -0300

    conhecendo sobre inputs e suas formas erradas de ser usado kkkk

---
+++ secao3/aula15.py
@@ -0,0 +1,9 @@
+# nome = input("Qual o seu nome: ")
+# print(f"Seu nome é {nome}")
+
+
+# a funcao input sempre retorna uma string
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+
+print(f"A soma entre {num1} e {num2} é {num1 + num2}")
