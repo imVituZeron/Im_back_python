490d34d6922c23f155ff32e69f491f1110744d23
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-07 08:30:47 -0300

    variaveis

---
+++ secao3/aula7.py
@@ -0,0 +1,11 @@
+nome_completo = "Vitor Santos"
+soma_mais_dois = 2+2
+
+int_um = int('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_mais_dois)
+
+
+idade = 23
+maior_de_idade = idade >= 18
+print(f"O {nome_completo} tem {idade}")
