5cb4131f1d19f76ea7df24bae356a3c84e5537c8
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-07 11:32:53 -0300

    aprendendo o basico sobre os condicionais if-elif-else

---
+++ secao3/aula16.py
@@ -0,0 +1,8 @@
+entrada = input("Voce que entrar ou sair? ")
+
+if entrada == "entrar":
+    print("Voce entrou no sistema")
+elif entrada == "sair":
+    print("Voce saiu do sistema")
+else:
+    print("Resposta invalida!")
