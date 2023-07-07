487ef9e974108fbab7afd240858e144fa2634a60
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-07 08:13:44 -0300

    vendo um pouco de typecasting

---
+++ secao3/aula6.py
@@ -0,0 +1,9 @@
+#conversao de tipos
+
+print(1+1)
+print("1" + str(1)) #passando um inteiro para string
+print(int("1") + int("1")) # passantos dois tipos de string para inteiro
+print(type(int("1") + int("1"))) # vendo o tipo de uma soma de dois typecasting de string para inteiro
+print(1 + float(2)) # typecasting de int para float
+print(bool(" ")) # Uma string typecasting para bool e True
+print(bool("")) # uma string vazia typecasting para bool e False
