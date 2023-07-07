3ef18a0631e1cfe581dad07702e9ffc0b24ed83f
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-07 07:56:29 -0300

    tipo booleano

---
+++ secao3/aula5.py
@@ -0,0 +1,7 @@
+print(20 == 20) # True pois 20 é igual a 20 (verdade)
+print(20 == 21) # False pois 20 não é igual a 21 (falso)
+
+print(True == True)
+print(True == False)
+print(type(True == False)) # mostra o type do valor da expressão
+
