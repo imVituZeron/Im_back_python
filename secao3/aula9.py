daec722cf77889db48021c04fc96e123b2acba2f
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-07 09:10:24 -0300

    vendo sobre os operadores aritmeticos

---
+++ secao3/aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print("Adição:", adicao)
+
+sub = 10 - 5
+print("Subtração:", sub)
+
+multplicacao = 10 * 100
+print("Multiplicação:", multplicacao)
+
+divisao = 10 / 2
+print("Divisão:", divisao)
+
+divisao_inteira = 10 // 2
+print("Divisão inteira:", divisao_inteira)
+
+exponenciacao = 2 ** 10
+print("Exponenciação:", exponenciacao)
+
+modulo = 10 % 2
+print("Modulo:", modulo)
