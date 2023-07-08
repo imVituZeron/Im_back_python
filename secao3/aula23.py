3c4966e085b331f6c61fe359a06a28e92b039982
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-08 16:52:01 -0300

    in, not in

---
+++ secao3/aula23.py
@@ -0,0 +1,13 @@
+#  0 1 2 3 4
+# -5-4-3-2-1 
+#  v i t o r
+
+
+nome = "vitor"
+
+print(nome[2])
+print(nome[-3])
+print('v' in nome) # retorna um valor booleano
+print("tor" in nome)
+print('v' not in nome) # Perguntando se 'v' nao esta em nome
+print("tor" not in nome) # Perguntando se 'tor' nao esta em nome
