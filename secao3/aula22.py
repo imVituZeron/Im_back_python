7a00b3fa6ef8cfd2fc498ea3808569f7f312fe82
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-08 16:42:02 -0300

    vendo um pouco sobre NOT

---
+++ secao3/aula22.py
@@ -0,0 +1,5 @@
+
+print(not 1) # 1 igual a True mas o not transforma ele em False
+print(not 0) # 0 igual a Flase mas o note transforma ele em True
+print(not "") # uma string fazia igual False o not inverte o valor para True
+print(not " ") # uma string com algo e igual True o not inverte o valor para False
