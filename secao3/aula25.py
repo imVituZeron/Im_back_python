2795351220ebcb5bb1ea4e59c393240c1abd6774
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-09 09:23:39 -0300

    modificacao de string com f-strings

---
+++ secao3/aula25.py
@@ -0,0 +1,6 @@
+variavel = "ABC345"
+print(f"{variavel}")
+#completando o restando da string que nao tiver caracteres os suficiente.
+#f '{[VARIAVEL]:[CARACTER][DIRECAO <^>][QUANTIDADE_DE_CARACTER]}'
+print(f"{variavel:+>6}")
+print(f"{variavel:+<6}")
