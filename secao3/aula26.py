5a1db99873255cd989dc0067c6d1b2c888a0063b
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-10 09:00:24 -0300

    vendo sobre fatiamento

---
+++ secao3/aula26.py
@@ -0,0 +1,8 @@
+variavel = "Hello World"
+# [INICIO:FINAL:STEP]
+print(variavel[3:5])
+print(variavel[3:])
+print(variavel[:5])
+print(variavel[:len(variavel):2])
+print(variavel[::-1])
+print(len(variavel))
