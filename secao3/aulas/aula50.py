591ce43bd5d4267451ca1854319a4082d20743b5
imVituZeron <vitorpaulasantos@gmail.com>
2023-08-04 09:33:51 -0300

    vendo um pouco mais sobre listas

---
+++ secao3/aulas/aula50.py
@@ -0,0 +1,10 @@
+lista = [10, 20, 33, 40]
+lista.append("VITOR")
+nome = lista.pop()
+print(lista, nome)
+lista.append(123)
+print(lista)
+
+# adicionando um valor no indice 0
+lista.insert(0, 5)
+print(lista)
