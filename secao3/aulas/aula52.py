9645d95640b010202aae9fd05cf65af0c2c36a8e
imVituZeron <vitorpaulasantos@gmail.com>
2023-08-07 08:33:06 -0300

    .

--- secao3/aulas/aula52.py
+++ secao3/aulas/aula52.py
@@ -1,5 +1,5 @@
 lista_a = ['Vitor', 'Jorge']
-# o metodoos .copy() serve para copiar um lista, pois se voce so colocar o sinal de = vai esta direcionando para a mesmo lugar de memoria
+# o metodos .copy() serve para copiar uma lista, pois se voce so colocar o sinal de = vai esta direcionando para a mesmo lugar de memoria
 lista_b = lista_a.copy() 
 lista_a[0] = 'Qualquer coisa' 
 print(lista_a)
