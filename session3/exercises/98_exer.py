09996e61129be1fb76932d2fc4bd46458be7a0e3
imVituZeron <vitorpaulasantos@gmail.com>
2023-12-08 23:13:13 -0300

    I finished script

--- session3/exercises/98_exer.py
+++ session3/exercises/98_exer.py
@@ -12,6 +12,7 @@ def second_digit(ten_digits=str, first_digit=int):
     second_number = (second_total_sum * 10) % 11
     second_number = second_number if second_number <= 9 else 0
     print(second_number)
+    print("---")
 
     return second_number
 
@@ -28,12 +29,20 @@ def first_digit(nine_digits=str):
     first_number = (first_total_sum * 10) % 11
     first_number = first_number if first_number <= 9 else 0
     print(first_number)
+    print("---")
 
     return first_number
 
 
-string_cpf = "74682489070"
-first_nine_digits = string_cpf[:9]
+user_cpf = "74682489070"
+first_nine_digits = user_cpf[:9]
 
 first_number = first_digit(first_nine_digits)
 second_number = second_digit(first_nine_digits, first_number)
+
+calculate_cpf = f"{first_nine_digits}{first_number}{second_number}"
+
+print(f"CPF was given by user: {user_cpf}")
+print(f"CPF was made by machine: {calculate_cpf}")
+
+print("valid!") if user_cpf == calculate_cpf else print("invalid!")
