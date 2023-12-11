b47a67f7a6e8cfa0c81a25a7478f4ed8fc02b5ee
imVituZeron <vitorpaulasantos@gmail.com>
2023-12-11 20:27:47 -0300

    Started with functions

---
+++ session4/105_class.py
@@ -0,0 +1,11 @@
+def imprimir(a, b, c):
+    print("varias vezes", a, b, c)
+    
+
+def saudacao(name='asdfasdf'):
+    print(f"Hello {name}")
+
+imprimir(1,2,3)
+saudacao('Vitor')
+saudacao()
+saudacao('Vitor')
